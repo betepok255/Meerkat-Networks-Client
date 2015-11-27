62cdbdc52404cd96377fa8e6b03b8ee12910d945
betepok255 <marvell@marvells-imac.local>
2015-11-28 00:45:48 +0200

    API

--- Meerkat Networks Client/LoginViewController.swift
+++ Meerkat Networks Client/LoginViewController.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2015 Meerkat Networks. All rights reserved.
 //
 
+import Alamofire
 import UIKit
 
 class LoginViewController: UIViewController {
@@ -13,6 +14,24 @@ class LoginViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+//        var wait = UIActivityIndicatorView(frame: CGRect(x: 0, y: 0, width: 50, height: 50))
+        
+//        self.viewActivityIndicator = UIView(frame: CGRect(x: x, y: y, width: width, height: height))
+//        self.viewActivityIndicator.backgroundColor = UIColor(red: 255.0/255.0, green: 204.0/255.0, blue: 51.0/255.0, alpha: 0.5)
+//        self.viewActivityIndicator.layer.cornerRadius = 10
+//        
+//        self.activityIndicator = UIActivityIndicatorView(frame: CGRect(x: 0, y: 0, width: 50, height: 50))
+//        self.activityIndicator.color = UIColor.blackColor()
+//        self.activityIndicator.hidesWhenStopped = false
+//        
+//        let titleLabel = UILabel(frame: CGRect(x: 60, y: 0, width: 200, height: 50))
+//        titleLabel.text = "Processing..."
+//        
+//        self.viewActivityIndicator.addSubview(self.activityIndicator)
+//        self.viewActivityIndicator.addSubview(titleLabel)
+//        
+//        self.view.addSubview(self.viewActivityIndicator)
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -22,12 +41,27 @@ class LoginViewController: UIViewController {
 
     @IBAction func OnClickLoginButton(sender: AnyObject) {
         
+//        Alamofire.request(.GET, "https://httpbin.org/get", parameters: ["foo": "bar"])
+//            .responseJSON { response in
+//
+////                print(response.request)  // original URL request
+////                print(response.response) // URL response
+////                print(response.data)     // server data
+////                print(response.result)   // result of response serialization
+//                
+//                if (response.response?.statusCode == 2050){
+//                    
+//                } else {
+//                    let alert = UIAlertController(title: "Error", message: "Login or password incorrect!", preferredStyle: UIAlertControllerStyle.Alert)
+//                    alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil))
+//                    self.presentViewController(alert, animated: true, completion: nil)
+//                }
+//        }
+        
+        
         let next = self.storyboard?.instantiateViewControllerWithIdentifier("RootViewController") as! RootViewController
         self.presentViewController(next, animated: true, completion: nil)
         
-//        let initialViewController = UIStoryboard(name: "Main", bundle:nil).instantiateInitialViewController() as! HomeRootViewController
-//        let appDelegate = (UIApplication.sharedApplication().delegate as! AppDelegate)
-//        appDelegate.window?.rootViewController = initialViewController
     }
 
 }
---
+++ Meerkat Networks Client/ScanNowViewController.swift
@@ -0,0 +1,9 @@
+//
+//  ScanNowViewController.swift
+//  Meerkat Networks Client
+//
+//  Created by Marvell on 27.11.15.
+//  Copyright © 2015 Meerkat Networks. All rights reserved.
+//
+
+import Foundation
